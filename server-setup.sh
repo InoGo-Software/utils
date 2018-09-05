0f8d2982387abad6605f8914eccd68089c89e994
Stefan Pahlplatz <stefanpahlplatz@inogo.nl>
2018-09-05 19:50:06 +0000

    Update server-setup.sh

--- server-setup.sh
+++ server-setup.sh
@@ -13,7 +13,7 @@ cp ./.zshrc ~/.zshrc
 sed -i 's/REPLACE_USER/'"$USER"'/g' ~/.zshrc
 
 # Install zsh autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 
 # Install zsh syntax highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
